90f2154e76ec723baee607389deeaa7c77f19531
Seth T <seth10@users.noreply.github.com>
2018-04-21 11:56:03 -0400

    Edit liri.js to a testable state

--- liri.js
+++ liri.js
@@ -11,40 +11,44 @@ var Spotify = require('node-spotify-api');
 
 var getMyTweets = function() {
     
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+    var client = new Twitter(keys.twitter);
 
-var params = { screen_name: 'Unique_African' };
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-       for(var i=0; i<tweets.length; i++){
-        console.log(tweets[i]),created_at;
-        console.log(' ');
-        console.log(tweets[i],text);
-       }
-    }
-});
+    var params = { screen_name: 'Unique_African' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for(var i=0; i<tweets.length; i++){
+                console.log(tweets[i].created_at);
+                console.log(' ');
+                console.log(tweets[i].text);
+            }
+        }
+    });
 }
 
 
 
-
-            
+var getSpotifySong = function() {
+    var spotify = new Spotify(keys.spotify);
     spotify.search({ type: 'track', query: 'All the Small Things'}, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-    console.log(data.tracks.items[0]);
-});
+        console.log(data.tracks.items[0]);
+    });
+}
         
 var pick = function (casaData, functionData) {
     switch (casaData) {
         case 'my-tweets' :
-            getMyTweets;
+            getMyTweets();
             
             break;
     
+        case 'spotify-this-song':
+            getSpotifySong();
+            break;
+        
         default:
         console.log('Liri Does not know that');
             
